f869ef52efb8539c97158f52c25433e3165d2f0f
Valentin Vic <valentin.vic@cgi.com>
2019-04-08 10:17:49 +0200

    Fix change token

--- src/main/java/fr/openent/moodle/Moodle.java
+++ src/main/java/fr/openent/moodle/Moodle.java
@@ -18,7 +18,7 @@ import java.text.ParseException;
 public class Moodle extends BaseServer {
 
 	public static String DIRECTORY_BUS_ADDRESS = "directory";
-	public static String WSTOKEN = "df92b3978e2b958e0335b2f4df505977";
+	public static String WSTOKEN = "ce7f30ccedfaaa24485629872671200a";
 	public static String WS_CREATE_FUNCTION = "local_entcgi_services_createcourse";
 	public static String WS_DELETE_FUNCTION = "core_course_delete_courses";
 	public static String WS_GET_USERCOURSES = "local_entcgi_services_usercourses";
